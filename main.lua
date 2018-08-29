2f9add311d9eba21b7bb9374a8c6afe2d1e0813c
Beppe Vanrolleghem <beppe.vanrolleghem@gmail.com>
2018-08-29 04:17:00 +0200

    started with drawing rect

---
+++ main.lua
@@ -0,0 +1,19 @@
+function love.load()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+	love.graphics.rectangle("fill", 1, 1, 30, 30)
+end
+
+function love.keypressed(key)
+
+end
+
+function love.keyreleased(key)
+
+end
